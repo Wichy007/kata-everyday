07b3fb594f3932e1acc0728af5565544d92c62af
naravit bunthap <whichy@naravits-MacBook-Air.local>
2023-09-14 15:49:38 +0700

    success wiat for refactor

---  longest_substring_of_unique_characters.test.ts
+++  longest_substring_of_unique_characters.test.ts
@@ -4,5 +4,11 @@ import { describe, test, expect  } from "@jest/globals";
 describe('longest substring of unique', () => {
     test('', () => {
         expect(longestSubstr("abcabcbb")).toBe(3)
+        expect(longestSubstr("bbbbb")).toBe(1)
+        expect(longestSubstr("pwwkew")).toBe(3)
+        expect(longestSubstr("abcdef")).toBe(6)
+        expect(longestSubstr("")).toBe(0)
+        expect(longestSubstr("au")).toBe(2)
+        expect(longestSubstr("dvdf")).toBe(3)
     })
 })
---  longest_substring_of_unique_characters.ts
+++  longest_substring_of_unique_characters.ts
@@ -1,10 +1,28 @@
-export default function longestSubstr(s: string): number {
 
-    // your code here
-    return 3;
+
+export default function longestSubstr(s: string): number {
+    const letterArray = s.split('');
+    let storageArraay = [];
+    let current = 0
+    let longest = 0
+
+    for(let i = 0; i < letterArray.length; i++){
+        for (let j = i; j < letterArray.length; j++){
+            if(storageArraay.some(letter => letter === letterArray[j])){
+                if(current > longest){
+                    longest = current
+                }
+                current = 0
+                storageArraay = []
+                break;
+            }
+            storageArraay.push(letterArray[j])
+            current++
+        }
+    }
+    return longest
 }
 
-longestSubstr("hello")
 
 
 
