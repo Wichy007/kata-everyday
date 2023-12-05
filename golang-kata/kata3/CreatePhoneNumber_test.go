38647397d6cbba38405ef5f4de92c5bd4e5fd370
wichy007 <naravitdansiriviroj@gmail.com>
2023-12-05 09:28:40 +0700

    solve probelm for test should return telephone number form

--- golang-kata/kata3/CreatePhoneNumber_test.go
+++ golang-kata/kata3/CreatePhoneNumber_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestKata3(t *testing.T) {
-	t.Run("", func(t *testing.T) {
+	t.Run("should return telephone number form", func(t *testing.T) {
 		actual := kata3.CreatePhoneNumber([10]uint{1, 2, 3, 4, 5, 6, 7, 8, 9, 0})
 		assert.Equal(t, "(123) 456-7890", actual)
 	})
