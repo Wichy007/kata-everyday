c4bf7789c5efda5a958fe68395b933e726db59d9
wichy007 <naravitdansiriviroj@gmail.com>
2023-12-05 09:28:04 +0700

    solve probelm for test should return telephone number form

--- golang-kata/kata3/CreatePhoneNumber.go
+++ golang-kata/kata3/CreatePhoneNumber.go
@@ -1,1 +1,20 @@
 package kata3
+
+func CreatePhoneNumber(numbers [10]uint) []string {
+	result := []string{}
+	for i := 0; i < 14; i++ {
+		if i == 0 {
+			result = append(result, "(")
+		} else if i == 4 {
+			result = append(result, ")")
+		} else if i == 5 {
+			result = append(result, " ")
+		} else if i == 9 {
+			result = append(result, "-")
+		} else {
+			// result = append(result, strconv.Itoa(numbers[i]))
+		}
+	}
+
+	return result
+}
--- golang-kata/kata3/CreatePhoneNumber_test.go
+++ golang-kata/kata3/CreatePhoneNumber_test.go
@@ -1,9 +1,15 @@
 package kata3_test
 
-import "testing"
+import (
+	"golang-kata/kata3"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestKata3(t *testing.T) {
 	t.Run("", func(t *testing.T) {
-
+		actual := kata3.CreatePhoneNumber([10]uint{1, 2, 3, 4, 5, 6, 7, 8, 9, 0})
+		assert.Equal(t, "(123) 456-7890", actual)
 	})
 }
